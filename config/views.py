07d35c397fc4926679acb7670a07ecd8dbeb7e1d
Davi Oliveira Matos Carvalho <221.14.039@EDU.gruposete.local>
2024-10-28 22:05:03 -0300

    first commit

---
+++ config/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+def lista_tarefas(request):
+    tarefas = ["Estudar Django", "Criar um projeto", "Revisar o conteúdo"]
+    return render(request, 'lista_tarefas.html', {'tarefas':tarefas})
